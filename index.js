a4aca117d14a1267b07bc4a5b41300d088ca11e3
Ariel Navas <arielnavas05@gmail.com>
2023-10-07 18:31:52 -0500

    Template Finished

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const closeBtn= document.querySelector("#close-btn");
 
 /*para el cambio de color del tema*/
 
-const themeToggler= document.querySelector(".theme");
+const themeToggler= document.querySelector(".theme-toggler");
 
 
 
@@ -15,6 +15,32 @@ menuBtn.addEventListener('click', () => {
     sideMenu.style.display= 'block';
 })
 
+/*Para el sidebar se oculte*/
 closeBtn.addEventListener('click', () =>{
     sideMenu.style.display='none'
+})
+
+/*cambiar el color*/ 
+themeToggler.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+})
+
+
+// llenar los datos en la tabla
+Orders.forEach(order =>{
+    const tr=document.createElement('tr');
+    const trContent=`
+    <td>${order.productName}</td>
+    <td>${order.productNumber}</td>
+    <td>${order.paymentStatus}</td>
+    <td class="${order.shipping === 'Declined' ? 'danger':
+        order.shipping === 'Pending' ? 'warning': 'primary'}">${order.shipping}</td>
+    <td class="primary">Details</td>`;
+
+    tr.innerHTML=trContent;
+    document.querySelector('table tbody').appendChild(tr);
 })
---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders= [
+    {
+        productName: 'Foldable mini drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Castle of ragnarok',
+        productNumber: '85632',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Kingsman suit',
+        productNumber: '85633',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Dualshock',
+        productNumber: '85634',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Master race',
+        productNumber: '85635',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ketchup',
+        productNumber: '85636',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
